5404344c9cc84bf116563da3329d9b438f54d4b4
jasonboru <jasonboru@gmail.com>
2017-05-13 18:53:08 -0400

    initial server setup

---
+++ sever.js
@@ -0,0 +1,55 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require(".models/Article.js");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// Use body parser with our app
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+// Make public a static dir
+app.use(express.static(process.cwd() + "/public"));
+
+// Database configuration with mongoose
+//mongoose.connect("mongodb://heroku_3xml4qth:mo8048sejk41r1uohtpi9q5vbo@ds045614.mlab.com:45614/heroku_3xml4qth");
+mongoose.connect("mongodb://localhost/mongoosearticles");
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection sucessful.");
+});
+
+//set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine"), "handlebars");
+
+// Import routes and give the server access to them.
+var router = express.Router();
+
+// Require routes file pass router object
+require("./config/routes")(router);
+
+// Have every request go through router middlewar
+app.use(router);
+
+//set port
+var port - process.env.PORT || 3001;
+
+//setup listener
+app.listen(port, function() {
+  console.log("app running on port " + port);
+});
